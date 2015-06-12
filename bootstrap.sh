9467ce0ae69886e564308d13793b4f3b9182101d
Nathan Chere <git@nathanchere.com.au>
2015-06-13 00:19:25 +0200

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#
+# nixstarter bootstrapper
+#
+# Copy/paste this into a terminal to get started:
+#
+#   wget -O - https://j.mp/nixstarterbootstrap >> bootstrap.sh && sudo chmod +777 bootstrap.ch && ./bootstrap.sh
+#
+
+sudo apt-get update
+sudo apt-get install -y -q git git-core
+cd /tmp
+rm -drf /tmp/nixstarter
+git clone -q https://github.com/nathanchere/nixstarter.git
+cd nixstarter
+
+# Edit the config file how you like it
+
+# then run.sh config.path
