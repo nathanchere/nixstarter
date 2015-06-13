b0c9441b8232ef8c32170ee6c5e09fc210d2c0eb
Nathan Chere <git@nathanchere.com.au>
2015-06-14 01:11:37 +0200

    Config script template

---
+++ config.sh
@@ -0,0 +1,75 @@
+#
+# For info on how to use this, read help.txt 
+#
+
+GITUSERNAME=someuser
+GITNAME=Some User
+GITEMAIL=someuser@pretend.mail
+
+return
+
+#
+# Items below this heading are to enable feature installs.
+# Comment out the ones you don't want. Note that order is important.
+# Do not, for example, try to install PyGame before Python
+#
+
+#########################################
+# UI stuff
+#########################################
+
+INSTALL_ZSH=1
+INSTALL_OHMYZSH=1
+
+#########################################
+# Config stuff
+#########################################
+
+CONFIGURE_GITHUB=1
+CONFIGURE_GITALIASES=1
+
+#########################################
+# Framework stuff
+#########################################
+
+INSTALL_BASE_MONO=1
+INSTALL_BASE_JAVA=1
+
+#########################################
+# Dev stuff
+#########################################
+
+INSTALL_NODEJS=1
+INSTALL_JSDEV=1
+
+INSTALL_CORECLR=1
+
+INSTALL_OCAML=1
+INSTALL_HAXE=1
+
+INSTALL_PYTHON=1
+INSTALL_PYGAME=1
+
+INSTALL_ERLANG=1
+INSTALL_ELIXIR=1
+
+INSTALL_GO=1
+
+INSTALL_D=1
+INSTALL_RUST=1
+
+INSTALL_JAVA=1
+INSTALL_SCALA=1
+INSTALL_CLOJURE=1
+
+INSTALL_PHP=1
+INSTALL_HASKELL=1
+INSTALL_RUBY=1
+
+## 
+# install mysql
+# install postgresql
+# install mongodb
+# install rabbitmq
+##
+# TODO: Install IDEs separately
