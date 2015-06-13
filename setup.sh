15f03876d11337f14d82befd9e5edf893dde3c25
Nathan Chere <git@nathanchere.com.au>
2015-06-14 01:13:40 +0200

    coreclr and java setup

--- setup.sh
+++ setup.sh
@@ -19,6 +19,22 @@ sudo apt-get update
 sudo apt-get install -qq git git-core mercurial
 sudo apt-get install -qq curl sharutils sed jq autoconf
 
+################################################
+# FRAMEWORKS / RUNTIMES
+################################################
+
+if [-n "$INSTALL_BASE_MONO"]; then
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+	echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+	sudo apt-get update
+	sudo apt-get install -qq mono-complete
+fi
+
+if [-n "$INSTALL_BASE_JAVA"]; then
+	sudo apt-get install -qq openjdk-6-jre openjdk-7-jre
+fi
+
+
 
 ################################################
 # GIT CONFIG
