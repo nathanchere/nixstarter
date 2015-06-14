f341fec8bb1b08e2c6791257270a9a5d45393c5f
Nathan Chere <git@nathanchere.com.au>
2015-06-14 10:24:55 +0200

    Replace overly presumptuous node bootstrap script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,18 @@
 
 INSTALLDIR=pwd
 
+drawHeader() {
+	echo " "
+	echo "#######################################################################"
+	echo "# $1"
+	echo "#######################################################################"	
+	echo " "
+}
+
+write() {
+	echo " -> $1..."
+}
+
 # TODO:
 # Tilda/quake - and add to autostart - and import config
 # Something like Launchy for Linux
@@ -12,16 +24,35 @@ INSTALLDIR=pwd
 # install github's `hub` helper
 
 ################################################
+# APTITUDE REPOS
+################################################
+
+drawHeader "Updating aptitude repositories"
+
+if [ -n "$INSTALL_BASE_MONO" ]; then
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+	echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+fi
+
+if [ -n "$INSTALL_NODEJS" ]; then
+	sudo add-apt-repository -y ppa:chris-lea/node.js
+fi
+
+sudo apt-get update -qq
+
+################################################
 # ESSENTIALS
 ################################################
 
-sudo apt-get update
+drawHeader "Installing essential utils"
 sudo apt-get install -qq git git-core mercurial
 sudo apt-get install -qq curl sharutils sed jq autoconf
 
 ################################################
 # MISC BUILD DEPENDENCIES
 ################################################
+
+drawHeader "Installing build dependencies"
 
 sudo apt-get install -qq -fix-missing build-essential libssh-dev m4 n
 sudo apt-get install -qq -fix-missing ncurses5-dev openssl libssl-dev unzip
@@ -34,15 +65,17 @@ sudo apt-get install -qq -fix-missing libunwind8
 ################################################
 
 if [ -n "$INSTALL_BASE_MONO" ]; then
-	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-	echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-	sudo apt-get update
+	drawHeader "Installing Mono runtime"
 	sudo apt-get install -qq mono-complete
 fi
 
 if [ -n "$INSTALL_BASE_JAVA" ]; then
+	drawHeader "Installing Java runtime"
 	sudo apt-get install -qq openjdk-6-jre openjdk-7-jre
 fi
+
+
+exit
 
 ################################################
 # GIT CONFIG
@@ -57,7 +90,7 @@ if [ -n "$CONFIGURE_GITHUB" ]; then
 fi
 
 if [ -n "$CONFIGURE_GITALIASES" ]; then
-	#TODO
+	echo "TODO: git aliases here"
 fi
 
 ################################################
@@ -77,8 +110,7 @@ fi
 # LANGUAGE RUNTIMES / SDKs / COMPILERS / etc
 ################################################
 
-if [ -n "$INSTALL_NODEJS" ]; then
-	curl -sL https://deb.nodesource.com/setup | sudo bash -
+if [ -n "$INSTALL_NODEJS" ]; then	
 	sudo apt-get install -qq nodejs
 	sudo chown -R $USER ~/.npm
 	sudo chown -R $USER ~/usr/lib/node_modules
