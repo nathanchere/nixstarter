cae934c56f93626a2c6937b7542d820798245cf1
Nathan Chere <git@nathanchere.com.au>
2015-06-15 03:03:45 +0200

    Force overwrite old SSH key

--- setup.sh
+++ setup.sh
@@ -101,6 +101,7 @@ if [ -n "$CONFIGURE_GITHUB" ]; then
 	git config --global user.name "$GITNAME"
 	git config --global user.email "$GITEMAIL"
 	mkdir ~/.ssh
+	rm -f ~/.ssh/id_rsa
 	echo -e 'y'|ssh-keygen -t rsa -b 4096 -C "$GITEMAIL" -f ~/.ssh/id_rsa -N ''
 	ssh-agent -c
 	ssh-add ~/.ssh/id_rsa
